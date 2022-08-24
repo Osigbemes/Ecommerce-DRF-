56b513fbe6974c23db74fc301473593b96a2cf06
Osinaike Gbemileke <osinaikegbemileke@gmail.com>
2022-08-24 15:55:29 +0100

    Created Product, Order tables

--- Customer/models.py
+++ Customer/models.py
@@ -33,6 +33,76 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     userName= models.CharField(max_length=200, unique=True)
     firstName = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=250, unique=True, blank=True)
     objects = CustomAccountManager()
     USERNAME_FIELD = 'userName'
-    REQUIRED_FIELDS = ['firstName']
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.userName
+
+class Product(models.Model):
+    productName = models.CharField(unique=True, max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    digital = models.BooleanField(default=False,null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.productName
+
+class Order(models.Model):
+	customer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+	date_ordered = models.DateTimeField(auto_now_add=True)
+	complete = models.BooleanField(default=False)
+	transaction_id = models.CharField(max_length=100, null=True)
+
+	def __str__(self):
+		return str(self.customer +'' + self.id)
+
+	@property
+	def shipping(self):
+		shipping = False
+		orderitems = self.orderitem_set.all()
+		for i in orderitems:
+			if i.product.digital == False:
+				shipping = True
+		return shipping
+
+	@property
+	def get_cart_total(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.get_total for item in orderitems])
+		return total
+
+	@property
+	def get_cart_items(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.quantity for item in orderitems])
+		return total
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.product + self.id
+        
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.customer
