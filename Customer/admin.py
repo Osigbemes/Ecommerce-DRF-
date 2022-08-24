54b0090bd81470e40af3b4d6ef71eec3ad8b1dbc
Osinaike Gbemileke <osinaikegbemileke@gmail.com>
2022-08-24 16:34:14 +0100

    Registered different tables in the admin dahsboard

--- Customer/admin.py
+++ Customer/admin.py
@@ -1,3 +1,10 @@
+from itertools import product
+from math import prod
 from django.contrib import admin
+from .models import User, Product, Order, OrderItem, ShippingAddress
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
